7f1f74b017a613d140cc293d7c87794c58a20bfa
AlejoVinluan <alejo.vinluan146@gmail.com>
2023-02-09 14:42:23 -0600

    Finished iterative DFS

--- Assignment1/Python/Main.py
+++ Assignment1/Python/Main.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 import Board
 from functions import findStar, printBoard, swapDown, swapLeft, swapRight, swapUp, DFS
 
@@ -8,16 +9,6 @@ class Main:
         exit(1)
     
     board = Board.Board(sys.argv[1])
-
-    print('original board')
-    print(printBoard(board.getState()))
-    boardCopy = board.getState().copy()
-    print('swapped down')
-    print(printBoard(swapDown(boardCopy)))
-    print('original board again')
-    print(printBoard(board.getState()))
-
-    print('up')
-    boardCopy = board.getState().copy()
-    print(printBoard(swapUp(boardCopy)))
+    DFS(copy.deepcopy(board))
+    
 
--- Assignment1/Python/functions.py
+++ Assignment1/Python/functions.py
@@ -1,3 +1,7 @@
+import copy
+
+goalState = [['7','8','1'],['6','*','2'],['5','4','3']]
+
 def printBoard(state):
     if not state:
         return "null\n"
@@ -17,15 +21,15 @@ def findStar(state):
     return [-1,-1]
 
 def swapUp(state):
-        starLocation = findStar(state)
-        if starLocation[0] - 1 >= 0:
-            cloneState = state
-            temp = cloneState[starLocation[0]-1][starLocation[1]]
-            cloneState[starLocation[0]-1][starLocation[1]] = '*'
-            cloneState[starLocation[0]][starLocation[1]] = temp
-            return cloneState
-        else:
-            return None
+    starLocation = findStar(state)
+    if starLocation[0] - 1 >= 0:
+        cloneState = state
+        temp = cloneState[starLocation[0]-1][starLocation[1]]
+        cloneState[starLocation[0]-1][starLocation[1]] = '*'
+        cloneState[starLocation[0]][starLocation[1]] = temp
+        return cloneState
+    else:
+        return None
         
 def swapDown(state):
     starLocation = findStar(state)
@@ -61,4 +65,33 @@ def swapRight(state):
         return None
 
 def DFS(board):
-    print(board.findStar())
+    stack = []
+    stack.append(copy.deepcopy(board.getState()))
+    depth = 0
+    
+    while(len(stack) != 0):
+        if depth >= 10:
+            print("Unable to find solution at depth 10.")
+            return
+        
+        currState = stack.pop(0)
+        if currState == goalState:
+            print("Goal state found!")
+            print("currState:", currState)
+            print("goalState:", goalState)
+            return
+        
+        print("depth", depth)
+        print(printBoard(currState))
+        
+        if(swapUp(copy.deepcopy(currState))):
+            stack.append(swapUp(copy.deepcopy(currState)))
+        if(swapDown(copy.deepcopy(currState))):
+            stack.append(swapDown(copy.deepcopy(currState)))
+        if(swapLeft(copy.deepcopy(currState))):
+            stack.append(swapLeft(copy.deepcopy(currState)))
+        if(swapRight(copy.deepcopy(currState))):
+            stack.append(swapRight(copy.deepcopy(currState)))
+            
+        
+        
